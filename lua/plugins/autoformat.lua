6b65d1789bb9847dc09b1c239f5e5893421bc5fb
rrffrl <wilkinsonleora@gmail.com>
2026-05-13 18:28:06 -0500

    init

---
+++ lua/plugins/autoformat.lua
@@ -0,0 +1,65 @@
+return {
+  -- Autoformat
+  'stevearc/conform.nvim',
+  event = { 'BufWritePre' },
+  cmd = { 'ConformInfo' },
+
+  keys = {
+    {
+      '<leader>f',
+      function()
+        require('conform').format {
+          async = true,
+          lsp_format = 'fallback',
+        }
+      end,
+      mode = '',
+      desc = '[F]ormat buffer',
+    },
+    -- Toggle autoformat
+    {
+      '<leader>tf',
+      function()
+        vim.g.disable_autoformat = not vim.g.disable_autoformat
+
+        if vim.g.disable_autoformat then
+          vim.notify 'Autoformat disabled'
+        else
+          vim.notify 'Autoformat enabled'
+        end
+      end,
+      desc = '[T]oggle auto[F]ormat',
+    },
+  },
+
+  opts = {
+    notify_on_error = false,
+
+    format_on_save = function(bufnr)
+      -- Global toggle
+      if vim.g.disable_autoformat then return end
+
+      local disable_filetypes = { c = true, cpp = true }
+
+      local lsp_format_opt
+      if disable_filetypes[vim.bo[bufnr].filetype] then
+        lsp_format_opt = 'never'
+      else
+        lsp_format_opt = 'fallback'
+      end
+
+      return {
+        timeout_ms = 500,
+        lsp_format = lsp_format_opt,
+      }
+    end,
+
+    formatters_by_ft = {
+      lua = { 'stylua' },
+
+      javascript = { 'prettier' },
+      html = { 'prettier' },
+      css = { 'prettier' },
+    },
+  },
+}
---
+++ lua/plugins/init.lua
@@ -0,0 +1,45 @@
+return {
+  {
+    'stevearc/oil.nvim',
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    opts = {},
+    -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+    lazy = false,
+  },
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter',
+    },
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {},
+  },
+  {
+    -- See `:help gitsigns` to understand what the configuration keys do
+    -- Adds git related signs to the gutter, as well as utilities for managing changes
+    'lewis6991/gitsigns.nvim',
+    opts = {
+      signs = {
+        add = { text = '+' },
+        change = { text = '~' },
+        delete = { text = '_' },
+        topdelete = { text = '‾' },
+        changedelete = { text = '~' },
+      },
+    },
+  },
+  { -- Add indentation guides even on blank lines
+    'lukas-reineke/indent-blankline.nvim',
+    -- Enable `lukas-reineke/indent-blankline.nvim`
+    -- See `:help ibl`
+    main = 'ibl',
+    opts = {},
+  },
+  {
+    'windwp/nvim-autopairs',
+    event = 'InsertEnter',
+    opts = {},
+  },
+}
